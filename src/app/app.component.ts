6062340941a5a9d44fb4f4b1879349471e2494ca
jfbarahonag <jfbarahonag@unal.edu.co>
2023-06-12 22:19:12 -0500

    calcualtor was implemented with sum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+type OperatorType = '+' | '-' | '/' | '*'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculator';
+
+  calc = {
+    A: 0,
+    B: 0,
+    result: 0,
+  };
+
+  calculate(operator: OperatorType) {
+    if (operator === '+') {
+      this.calc.result = this.calc.A + this.calc.B
+    } else if (operator === '-') {
+      this.calc.result = this.calc.A - this.calc.B
+    } else if (operator === '/') {
+      this.calc.result = this.calc.A / this.calc.B
+    } else if (operator === '*') {
+      this.calc.result = this.calc.A * this.calc.B
+    }
+  }
 }
