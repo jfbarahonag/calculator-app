8c0035d9af655f99a9d37c44d7f1956f05b083a6
JUAN FELIPE BARAHONA GONZALEZ <jbarahona@xm.com.co>
2023-06-13 23:28:59 -0500

    formatting code

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,22 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 type OperatorType = '+' | '-' | '/' | '*';
-const operations: {[key in OperatorType]: (a: number, b: number) => number} = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '/': (a, b) => a / b,
-  '*': (a, b) => a * b,
-};
+const operations: { [key in OperatorType]: (a: number, b: number) => number } =
+  {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '/': (a, b) => a / b,
+    '*': (a, b) => a * b,
+  };
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-
 export class FormComponent {
   @Output() newResult = new EventEmitter<number>();
-  
+
   a: number = 0;
   b: number = 0;
 
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.css'],
 })
 export class ResultComponent {
   @Input() value: number = 0;
